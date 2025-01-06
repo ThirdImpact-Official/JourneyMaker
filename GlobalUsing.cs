3848030d065caab2a5419605b76ac2803985fd0b
Quentin Courcelles <78923290+AshThrax@users.noreply.github.com>
2025-01-06 20:22:02 +0100

    initialisation

---
+++ GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using SoundBoard.Models;
+global using SoundBoard.Repository;
+global using SoundBoard.Extension_Methodes;
+global using SoundBoard.Service.Interface;
+global using SoundBoard.Service.Exception;
